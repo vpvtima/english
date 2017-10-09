6b8ca3189bce7f894763b9ca759eb56e664073d9
Vpv Tima <vpv.tima@gmail.com>
2017-10-09 10:48:25 +0300

    Create magento.php

---
+++ eng/magento.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'sales'=>'продажи',
+    'orders'=>'заказы',
+    'invoices'=>'счета-фактуры',
+    'checks'=>'чеки',
+    'shipments'=>'отправки',
+    'transactions'=>'операции'
+];

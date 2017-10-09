73bfafc470a6450ccc402f69335e23f8781f781c
Vpv Tima <vpv.tima@gmail.com>
2017-10-09 10:56:59 +0300

    Update magento.php

--- eng/magento.php
+++ eng/magento.php
@@ -1,9 +1,16 @@
 <?php
 return [
+    'dashboard'=>'приборная панель',
     'sales'=>'продажи',
     'orders'=>'заказы',
     'invoices'=>'счета-фактуры',
     'checks'=>'чеки',
     'shipments'=>'отправки',
-    'transactions'=>'операции'
+    'transactions'=>'операции',
+    'billing'=>'выручка',
+    'agreements'=>'соглашения',
+    'terms'=>'сроки',
+    'conditions'=>'условия',
+    'gift'=>'подарок',
+    'wrapping'=>'упаковка',
 ];
